019136179eeecf1dc8c03fb2e645750776747987
Darsh kumar <116168977+darsh609@users.noreply.github.com>
2024-04-11 02:01:35 +0530

    add-dk-1

---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+            <div>
+            VISALTA
+            </div>
+            <div>
+                nitw
+            </div>
+            
+        </div>
+        <div>
+            <div>
+                Home
+            </div>
+            <div>
+                Explore
+            </div>
+            <div>
+                About
+            </div>
+        </div>
+        <div>
+            Login
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar
