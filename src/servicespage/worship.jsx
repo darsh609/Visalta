8f3ee31ecec66c70510c6548953a5cc0b91b1f7f
Darsh kumar <116168977+darsh609@users.noreply.github.com>
2024-06-30 23:31:23 +0530

    darsh-30june

--- src/servicespage/worship.jsx
+++ src/servicespage/worship.jsx
@@ -4,7 +4,21 @@ import { Link, Navigate, useNavigate } from 'react-router-dom';
  const Worship = () => {
   const navigate=useNavigate();
   return (
-    <div>Worship</div>
+    <div>
+      {
+        workship.map((ele,i)=>(
+          <div key={i}>
+            <div>
+            {ele.GodName}
+            </div>
+            
+
+          </div>
+        ))
+
+        
+      }
+    </div>
   )
 }
 export default Worship
