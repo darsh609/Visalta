fc3fd53ec484abb00564051daea329af7d6c388e
Vivek Bohra <bohravivek1503@gmail.com>
2024-06-30 23:21:22 +0530

    avbk

--- src/servicespage/worship.jsx
+++ src/servicespage/worship.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import worship from '../data/worship.json'
+import workship from '../data/workship.json'
 import { Link, Navigate, useNavigate } from 'react-router-dom';
  const Worship = () => {
   const navigate=useNavigate();
