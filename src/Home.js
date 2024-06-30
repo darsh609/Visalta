35278b4988f3136f43013728938fdee1d6b0f471
Darsh kumar <116168977+darsh609@users.noreply.github.com>
2024-06-30 23:16:02 +0530

    DARSH-17

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ export const Home = () => {
         
           <img src="./assets/IMG_0636.jpg" className="opacity-50"></img>
             <div className="h-8 bg-yellow-400 absolute bg-opacity-35 w-full top-8 text-center py-[2.8px] hover:text-center">
-              Announcement
+              Announcement-DARSH KUMAR
              </div>
           <button className="absolute top-90 left-1/3 font-serif right-1/3 bottom-40 border rounded-full text-2xl" >
           Navigating students life
