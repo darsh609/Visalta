27f7069aaf51d2baedd19602ee818781090aec87
Darsh kumar <116168977+darsh609@users.noreply.github.com>
2024-09-08 02:52:46 +0530

    8 sept

--- src/servicespage/foodfil.jsx
+++ src/servicespage/foodfil.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { filterData } from '../datas/filters'
 import { Sidebar } from './Sidebar'
+import food from '../datas/food.json'
 const Foodfil = () => {
 
     const[restaurants,setrest]=useState([]) 
@@ -21,6 +22,52 @@ const Foodfil = () => {
         
             
 
+ </div>
+ <div>
+ {food.map((ele,i)=>(
+    <div key={i}>
+
+    <div className=''>
+    <img src={ele.Img}></img>
+      {/* IMage */}
+      {/* <img src="icon1.png"> </img>  */}
+    </div>
+
+
+    <div>
+      {/* Name */}
+        {ele.Name}
+        <div className='  bg-blue-500'>
+          {/* arrow  for visit */}
+       <a href={ele.Visit} target="_blank"> 
+       {/* arrow */}
+       click here
+       </a>
+          </div>
+
+          <div>
+            {ele.Timing}
+            </div>
+        
+      
+        </div>
+
+
+    <div className=''>
+      {/* tags */}
+        <div className=''>
+        {ele.filters + " "}
+        </div>
+       
+     </div>
+
+
+      </div>
+
+    
+  ))
+
+  }
  </div>
 
 
