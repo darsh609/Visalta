e99a7189d15b43dd5f2304943e38ebb153ef64c5
ashishty23 <149870253+ashishty23@users.noreply.github.com>
2024-10-10 01:21:25 +0530

    tyagi

--- src/servicespage/FeatureCard.jsx
+++ src/servicespage/FeatureCard.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Eatables_Card from "../assets/Eatables_Card.png";
 import Travel_Card from "../assets/Travel_Card.png";
+import { motion, useAnimation } from 'framer-motion';
+import { Power4 } from 'gsap/all';
+
 
 const FeatureCard = () => {
+    
+       const cards = [useAnimation() , useAnimation()];
+
+       const handleHover = (index)=>{
+        cards[index].start({y: "0"})
+       }
+
+       const handleHoverEnd = (index)=>{
+        cards[index].start({y: "100%"})
+       }
+
+      
+    
+    
   return (
     <div className='w-full py-20 bg-[#D3D3CB] rounded-tl-3xl rounded-tr-3xl text-black'>
         <div className='w-full px-20 border-b-[1px] border-[#a1b562] pb-20'>
@@ -13,20 +30,48 @@ const FeatureCard = () => {
 
       <div className='px-20'>
       <div className="cards w-full flex gap-10 mt-10">
-                <div className="cardcontainer  relative w-1/2 h-[75vh] ">
-                    <h1 className='absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#7A871E] z-[10] font-["Founders_Grotesk_Condensed"] font-semibold text-8xl leading-none uppercase'>VenueSmart</h1>
-                    {"venueSmart".split('').map((item, index)=> <span>{item}</span>)}
+            <motion.div 
+            onHoverStart = {()=> handleHover(0)} 
+            onHoverEnd = {()=> handleHoverEnd(0)} 
+            className="cardcontainer  relative w-1/2 h-[75vh]">
+               <h1 className='absolute flex overflow-hidden right-0 translate-x-1/2 top-1/2 -translate-y-1/2 text-[#BCECE0] z-[10] font-["Founders_Grotesk_Condensed"] font-bold text-8xl leading-none tracking-tight uppercase'>
+                    {"venueSmart".split('').map((item, index)=> (
+                    <motion.span 
+                    initial={{y:"100%"}} 
+                    animate={cards[0]}
+                    transition={{ease: [0.22, 1, 0.36, 1], delay: index*.05}} 
+                    className='inline-block'>
+                        {item}
+                    </motion.span>
+                    ))}
+                    </h1>
+
                     <div className='card w-full h-full rounded-xl card overflow-hidden'>
                         <img className='w-full h-full bg-cover' src={Eatables_Card} alt=""  />
                     </div>
-                </div>
-                <div className="cardcontainer relative w-1/2 h-[75vh]">
-                <h1 className='absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-[#7A871E] z-[10] font-["Founders_Grotesk_Condensed"] font-semibold text-8xl leading-none uppercase'>Visit</h1>
-                {"visit".split('').map((item, index)=> <span>{item}</span>)}
+                </motion.div>
+
+                <motion.div 
+
+                onHoverStart = {()=> handleHover(1)} 
+                onHoverEnd = {()=> handleHoverEnd(1)} 
+
+                className="cardcontainer relative w-1/2 h-[75vh]">
+                <h1 className='absolute flex overflow-hidden right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-[#BCECE0] z-[10] font-["Founders_Grotesk_Condensed"] font-bold text-8xl leading-none tracking-tight uppercase'>
+                    {"visit".split('').map((item, index)=> (
+                     <motion.span 
+                     initial={{y:"100% "}} 
+                     animate={cards[1]}
+                     transition={{ease: [0.22, 1, 0.36, 1], delay: index*.05, scale: 5}} 
+                     className='inline-block'>
+                         {item}
+                     </motion.span>
+                     ))}
+                </h1>
                     <div className='card w-full h-full rounded-xl overflow-hidden'>
                         <img className='w-full h-full bg-cover' src={Travel_Card} alt="" />
                     </div>
-                </div>
+                </motion.div>
             </div>
       </div>
        
