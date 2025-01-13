d97936747960fc8e58ea1b7f5956d7670b03e5ee
ashishty23 <149870253+ashishty23@users.noreply.github.com>
2025-01-14 01:49:25 +0530

    insighgt_Cards

--- src/insights/insighthome.jsx
+++ src/insights/insighthome.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SplashCursor from './insight'
 import InsightPage from './InsightPage'
-
+import Footer from '../Footer'
 function Insighthome() {
   return (
     <div>
         <SplashCursor/>
         <InsightPage/>
+        <Footer/>
     </div>
   )
 }
